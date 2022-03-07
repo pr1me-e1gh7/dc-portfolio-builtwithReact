e769ae20021ef7ff4391e98876a4591bfbaf9200
dcaro1996 <dcaro1996@yahoo.com>
2022-03-07 13:54:47 -0500

    removed unused form+button for deploy

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,7 +1,5 @@
 import React from "react";
 import bkgd from "../images/contact.gif";
-import Form from 'react-bootstrap/Form'
-import Button from 'react-bootstrap/Button'
 // import 'bootstrap/dist/css/bootstrap.min.css'
 
 export default function Contact () {
